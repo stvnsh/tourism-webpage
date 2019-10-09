2b514c05440519de1f227f9e4a7527dbc08b744f
stvnsh <stvnsh@163.com>
2019-10-09 17:30:09 +0800

    2019年10月9日17:30:02

--- js/index/index.js
+++ js/index/index.js
@@ -1,11 +1,12 @@
 var viewHeight = $(window).height();
 $(".container").css("height", viewHeight);
+$(".swiper-slide").css("height", viewHeight*0.6)
 
 var width = document.body.clientWidth; 
 $(".container").css("max-width", width);
 $("body").css("max-width", width);
 
-var swiper = new Swiper('.swiper-container', {
+/* var swiper = new Swiper('.swiper-container', {
 	effect: 'coverflow',
 	grabCursor: true,
 	centeredSlides: true,
@@ -18,5 +19,15 @@ var swiper = new Swiper('.swiper-container', {
 		depth: 60,
 		modifier: 1,
 		slideShadows : true,
+	},
+}); */
+
+var swiper = new Swiper('.swiper-container', {
+	spaceBetween: 30,
+	effect: 'fade',
+	autoplay: true,
+	pagination: {
+		el: '.swiper-pagination',
+		clickable: true,
 	},
 });
